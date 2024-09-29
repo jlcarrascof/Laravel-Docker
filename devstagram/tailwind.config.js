210d8299d5643abb45330d013186f3b1896351d5
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-29 10:14:26 -0400

    Refactor tailwind.config.js to include blade.php files in content

--- devstagram/tailwind.config.js
+++ devstagram/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./resources/**/*.blade.php"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
