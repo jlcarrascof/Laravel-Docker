320f7e0fc2528d81e3a926ce08687fa70d5bfa90
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-06 12:31:02 -0400

    Refactor app.blade.php to remove unnecessary code and fix formatting

--- devstagram/resources/views/layouts/app.blade.php
+++ devstagram/resources/views/layouts/app.blade.php
@@ -6,8 +6,9 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
     </head>
     <body>
+
+    </body>
+
+</html>
