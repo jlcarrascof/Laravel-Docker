392f54d0a80a5504be568a7cc1aaac87f6c03e0b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-06 13:00:04 -0400

    Refactor app layout to update page title and add dynamic title

--- devstagram/resources/views/layouts/app.blade.php
+++ devstagram/resources/views/layouts/app.blade.php
@@ -4,24 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Devstagram - @yield('title')</title>
 
     </head>
 
     <body>
 
-        <nav>
-            <a href="/">Homepage</a>
-            <a href="/about">About Us</a>
-            <a href="/shop">Shop</a>
-            <a href="/contact">Contact</a>
-        </nav>
-
-        <h1>@yield('title')</h1>
-
-        <hr>
-
-        @yield('content')
     </body>
 
 </html>
