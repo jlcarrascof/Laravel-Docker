157f5a40f545a0a0746bf5f4fa2de37fb3e90c70
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-06 23:25:54 -0400

    Refactor app layout to update page title font style

--- devstagram/resources/views/layouts/app.blade.php
+++ devstagram/resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
         </header>
 
         <main class="container mx-auto mt-10">
-            <h2>
+            <h2 class="font-black text-center text-3xl mb-10">
                 @yield('title')
             </h2>
 
