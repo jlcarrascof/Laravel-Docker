a1ed4e68e8fae0e539aaad3bf9d7add71ef4ccde
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-10 13:31:23 -0400

    Refactor create account page to include username input field

--- devstagram/resources/views/auth/register.blade.php
+++ devstagram/resources/views/auth/register.blade.php
@@ -19,10 +19,10 @@
                     <input id="name" name="name" type="text" placeholder="Your name" class="border p-3 w-full rounded-lg" />
                 </div>
                 <div class="mb-5">
-                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
                         Username
                     </label>
-                    <input id="name" name="name" type="text" placeholder="Username" class="border p-3 w-full rounded-lg" />
+                    <input id="username" name="username" type="text" placeholder="Username" class="border p-3 w-full rounded-lg" />
                 </div>
             </form>
         </div>
