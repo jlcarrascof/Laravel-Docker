6aab17c94adfea15b25101499f6f653599f6a699
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-10 18:59:26 -0400

    Refactor create account page layout to include updated registration image

--- devstagram/resources/views/auth/register.blade.php
+++ devstagram/resources/views/auth/register.blade.php
@@ -5,9 +5,9 @@
 @endsection
 
 @section('content')
-    <div class="md:flex md:justify-center">
-        <div class="md:w-4/12">
-            <img src="{{ asset('img/registrar.png') }}" alt="Register on DevStagram image">
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{ asset('img/registrar.jpg') }}" alt="Register on DevStagram image">
         </div>
 
         <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
