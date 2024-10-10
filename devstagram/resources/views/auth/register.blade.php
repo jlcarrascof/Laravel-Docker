60a75adc94b902dc072f887a04d434fbac75306a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-10 14:18:01 -0400

    Refactor create account page to include username and email input fields

--- devstagram/resources/views/auth/register.blade.php
+++ devstagram/resources/views/auth/register.blade.php
@@ -18,11 +18,19 @@
                     </label>
                     <input id="name" name="name" type="text" placeholder="Your name" class="border p-3 w-full rounded-lg" />
                 </div>
+
                 <div class="mb-5">
                     <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
                         Username
                     </label>
                     <input id="username" name="username" type="text" placeholder="Username" class="border p-3 w-full rounded-lg" />
+                </div>
+
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input id="email" name="email" type="email" placeholder="Your Email" class="border p-3 w-full rounded-lg" />
                 </div>
             </form>
         </div>
