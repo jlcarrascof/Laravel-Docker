1bdd1f86fe383c6e28f82a98e25681aaa5fe7702
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-10 12:28:29 -0400

    Refactor route for create account page to use RegisterController

--- devstagram/resources/views/auth/register.blade.php
+++ devstagram/resources/views/auth/register.blade.php
@@ -3,3 +3,13 @@
 @section('title')
     Register on DevStagram
 @endsection
+
+@section('content')
+    <div class="md:flex">
+        <div class="md:w-1/2">
+            <p>Image here</p>
+        </div>
+
+        
+    </div>
+@endsection
