42b9e1a2efe0c77c8122ef546ede18a5fed29548
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-14 09:57:52 -0400

    Refactor create account page layout to include a POST route for account creation

--- devstagram/routes/web.php
+++ devstagram/routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 });
 
 Route::get('/create-account', [RegisterController::class, 'index'] );
-Route::post('/create-account', [RegisterController::class, 'index'] );
+Route::post('/create-account', [RegisterController::class, 'store'] );
